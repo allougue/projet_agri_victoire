3168a66947febfa12c12b458f914ba85057d9175
Allougue <allouguesognonvi@gmail.com>
2026-09-21 17:02:44 +0100

    Ajout de STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "connexion"
